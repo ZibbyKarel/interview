41e5145a504580e9f23113b7ec2fb83136ee7fd1
Karel Zibar <zibbykarel@gmail.com>
2023-03-05 17:19:10 +0100

    You should have trusted me.

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { getRates } from './middlewares/getRates';
+import morgan from 'morgan';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3001;
 
 const app = express();
+
+app.use(morgan('short'));
 
 app.get('/', (req, res) => {
 	res.send({ message: 'Hello API' });
